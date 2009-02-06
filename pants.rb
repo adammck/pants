96841c154187b149fd618a0d30dcdfbff95056eb
Adam Mckaig <adam.mckaig@gmail.com>
2009-02-06 12:21:39 -0500

    initial commit

---
+++ pants.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+# vim: noet
+
+
+require "open-uri"
+require "rss"
+require "erb"
+
+
+class Pants
+	def initialize(args, stdin)
+		if args.length == 2
+			@tmpl = File.get_contents(args[1])
+			@feed = args[0]
+
+		elsif args.length == 1
+			@feed = args[0]
+			@tmpl = TMPL
+
+		else
+			raise ArgumentError
+		end
+	end
+
+	def run
+		puts begin
+			@rss = open(@feed) { |s| s.read }
+			@data = RSS::Parser.parse(@rss)
+			ERB.new(@tmpl).result(binding)
+
+		rescue OpenURI::HTTPError => err
+			fail "Error fetching RSS feed"
+
+		rescue RSS::NotWellFormedError
+			fail "Malformed RSS feed"
+		end
+	end
+	
+	private
+	
+	def fail(text)
+		"<div class='feed-error'>#{text}</div>"
+	end
+end
+
+
+Pants::TMPL = <<EOT
+<div class="feed">
+	<ul><% @data.items.each do |item| %>
+		<li>
+			<h3><a href="<%= item.link %>"><%= item.title %></a></h3>
+			<div class="date"><%= item.date %></div>
+			<p><%= item.description %></p>
+		</li><% end %>
+	</ul>
+</div>
+EOT
+
+
+Pants.new(ARGV, STDIN).run
